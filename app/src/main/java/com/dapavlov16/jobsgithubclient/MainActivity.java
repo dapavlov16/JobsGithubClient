c173c55e8fefa222a9bc7a155488a7301a6a38c1
Dmitriy <dapavlov16@yandex.ru>
2019-01-30 17:30:46 +0300

    test

---
+++ app/src/main/java/com/dapavlov16/jobsgithubclient/MainActivity.java
@@ -0,0 +1,13 @@
+package com.dapavlov16.jobsgithubclient;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/dapavlov16/jobsgithubclient/Vacancy.java
@@ -0,0 +1,10 @@
+package com.dapavlov16.jobsgithubclient;
+
+public class Vacancy {
+    private String id;
+    private String url;
+    private String title;
+    private String description;
+
+
+}
