7456f1373fe2d2e86836328b00a08f06ce674aa8
Dmitriy <dm.glagolev@gmail.com>
2019-02-06 19:22:47 +0300

    fix bottomMargin of recview

--- app/src/main/java/com/dapavlov16/jobsgithubclient/ui/VacanciesFragment.java
+++ app/src/main/java/com/dapavlov16/jobsgithubclient/ui/VacanciesFragment.java
@@ -1,9 +1,7 @@
 package com.dapavlov16.jobsgithubclient.ui;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
